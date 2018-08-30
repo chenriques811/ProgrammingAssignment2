64707b30dbf7561fe17de396f8b1fa153d7d20b2
chenriques811 <42845430+chenriques811@users.noreply.github.com>
2018-08-30 13:44:59 -0400

    Update cachematrix.R
    Homework assignment week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## function to store a matrix that can be inverted
+## assume that the matrix is inverable (square)
 
 makeCacheMatrix <- function(x = matrix()) {
 
+     ## set value of inverted matrix to NULL
+     imatrix <- NULL
+     
+     ## define the function to assign the matrix and the inverted matrix to the parent environment
+     set <- function(y) {
+          x <<- y
+          imatrix <<- NULL
+     }
+     
+     ## define the function that will retrieve the input matrix
+     get <- function() x
+     
+     ## define function to assign inverted matrix to parent environment
+     setinv <- function(z) imatrix <<- z
+     
+     ## define function to retrieve inverted matrix
+     getinv <- function() imatrix
+     
+     ## create a list of functions
+     list(set = set,
+          get = get,
+          setinv = setinv,
+          getinv = getinv)
+     
 }
 
 
-## Write a short comment describing this function
+## function to retrieve the inverse of the stored matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+     
+     ## check if inverted matrix exists in parent environment
+     imatrix <- x$getinv()
+     
+     ## if an inverted matrix exists retrieve it from the parent environment
+     if (!is.null(imatrix)) {
+          return(imatrix)
+     }
+     
+     ## get the matrix cached in makeCacheMatrix
+     data <- x$get()
+     
+     ## invert the matrix
+     imatrix <- solve(data, ...)
+     
+     ## return the inverse to the parent environment
+     x$setinv(imatrix)
+     imatrix
+     }
